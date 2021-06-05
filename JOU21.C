3e92579df059b8e452a78485c201f7bd951addfb
Ansh Galani <85383708+AnshGalani@users.noreply.github.com>
2021-06-05 14:41:19 +0530

    Add files via upload

---
+++ JOU21.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+void chang(char []);
+
+int main()
+{
+    int r;
+    char s[100];
+    clrscr();
+    printf("Enter a string");
+    gets(s);
+    chang(s);
+    getch();
+}
+
+void chang(char a[])
+{
+    char ch;
+    int c=0;
+    while(a[c] !='\0')
+    {
+        ch=a[c];
+        if(ch>='A' && ch<='Z')
+            a[c]=a[c]+32;
+        else if (ch>='a' && ch<='z')
+            a[c]=a[c]-32;
+        c++;
+    }
+    printf("\nChang case is %s",a);
+}
